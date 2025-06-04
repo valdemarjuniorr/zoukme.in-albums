2cf7d3ffde198ab66b3cd14124f961d343e66b0f
Valdemar Jr <valdemarjuniorr@gmail.com>
2025-06-04 08:52:55 -0300

    Update date formatting and enhance event details layout; improve styling and add countdown functionality

--- src/main/java/in/zoukme/zouk_album/utils/DateUtils.java
+++ src/main/java/in/zoukme/zouk_album/utils/DateUtils.java
@@ -8,23 +8,23 @@ public final class DateUtils {
   /** This method returns a descriptive date as 21/Fev/2025 */
   public static String getDescriptiveDate(LocalDate date) {
     return String.format(
-        "%d/%s/%d", date.getDayOfMonth(), getDescriptiveMonth(date.getMonth()), date.getYear());
+        "%d de %s de %d", date.getDayOfMonth(), getDescriptiveMonth(date.getMonth()), date.getYear());
   }
 
   private static String getDescriptiveMonth(Month month) {
     return switch (month) {
-      case JANUARY -> "Jan";
-      case FEBRUARY -> "Fev";
-      case MARCH -> "Mar";
-      case APRIL -> "Abr";
-      case MAY -> "Mai";
-      case JUNE -> "Jun";
-      case JULY -> "Jul";
-      case AUGUST -> "Ago";
-      case SEPTEMBER -> "Set";
-      case OCTOBER -> "Out";
-      case NOVEMBER -> "Nov";
-      case DECEMBER -> "Dez";
+      case JANUARY -> "Janeiro";
+      case FEBRUARY -> "Fevereiro";
+      case MARCH -> "Março";
+      case APRIL -> "Abril";
+      case MAY -> "Maio";
+      case JUNE -> "Junho";
+      case JULY -> "Julho";
+      case AUGUST -> "Agosto";
+      case SEPTEMBER -> "Setembro";
+      case OCTOBER -> "Outubro";
+      case NOVEMBER -> "Novembro";
+      case DECEMBER -> "Dezembro";
     };
   }
 }
