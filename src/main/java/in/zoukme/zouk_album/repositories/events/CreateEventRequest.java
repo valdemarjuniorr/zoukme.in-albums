e9b375c0a76bbee76ee3f7d16d10824ecd0919c4
Valdemar Jr <valdemarjuniorr@gmail.com>
2026-06-22 16:15:05 -0300

    fix bug when create an event without past events

--- src/main/java/in/zoukme/zouk_album/repositories/events/CreateEventRequest.java
+++ src/main/java/in/zoukme/zouk_album/repositories/events/CreateEventRequest.java
@@ -1,12 +1,13 @@
 package in.zoukme.zouk_album.repositories.events;
 
+import in.zoukme.zouk_album.domains.Event;
+import in.zoukme.zouk_album.domains.payments.Package;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-
-import in.zoukme.zouk_album.domains.Event;
-import in.zoukme.zouk_album.domains.payments.Package;
+import java.util.Objects;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,5 +28,16 @@ public record CreateEventRequest(
 
   public List<Package> toPackages(AggregateReference<Event, Long> eventSaved) {
     return packages.stream().map(pack -> pack.toDomain(eventSaved)).toList();
+  }
+
+  /** to remove bug when a file is not uploaded in the interface * */
+  @Override
+  public List<MultipartFile> pastEvents() {
+    if (Objects.nonNull(pastEvents)) {
+      return pastEvents.stream()
+          .filter(f -> !f.isEmpty() && StringUtils.hasText(f.getOriginalFilename()))
+          .toList();
+    }
+    return null;
   }
 }
